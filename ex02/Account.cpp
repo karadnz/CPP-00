e6a5995ba00d3ff108919fe61770027a4468c0a7
Mustafa Karadeniz <mkaraden@student.42istanbul.com.tr>
2023-07-30 18:37:55 +0300

    updated

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,12 +6,18 @@
 /*   By: mkaraden <mkaraden@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 15:37:54 by mkaraden          #+#    #+#             */
-/*   Updated: 2023/07/30 16:27:11 by mkaraden         ###   ########.fr       */
+/*   Updated: 2023/07/30 18:36:51 by mkaraden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <iostream>
+#include <iomanip>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount= 0;
+int Account::_totalNbDeposits= 0;
+int Account::_totalNbWithdrawals = 0;
 
 //getters
 int	Account::getNbAccounts( void )
@@ -34,21 +40,38 @@ void	Account::displayAccountsInfos( void )
 {
 	_displayTimestamp();
 	std::cout
-	<< "index:" << this->_accountIndex << ";"
-	<< "amount:" << this->_accountIndex << ";"
-	<< "created:" << this->_accountIndex << ";"
-	<< "index:" << this->_accountIndex << ";"
-	
+	<< "accounts:" <<  getNbAccounts()<< ";"
+	<< "total:" << getTotalAmount() << ";"
+	<< "deposits:" << getNbDeposits() << ";"
+	<< "withdrawals:" << getNbWithdrawals()
+	<< std::endl;
 }
 
 
 //construct, destruct
 Account::Account( int initial_deposit )
 {
+	this->_accountIndex = _nbAccounts;
+	_nbAccounts++;
+	this->_amount = initial_deposit;
+	_totalAmount += initial_deposit;
+
+	_displayTimestamp();
+	std::cout
+	<< "index:" <<  this->_accountIndex << ";"
+	<< "amount:" << this->_amount << ";"
+	<< "created"
+	<<std::endl;
 	
 }
 Account::~Account( void )
 {
+	_displayTimestamp();
+	std::cout
+	<< "index:" <<  this->_accountIndex << ";"
+	<< "amount:" << this->_amount << ";"
+	<< "closed"
+	<<std::endl;
 	
 }
 
@@ -56,22 +79,80 @@ Account::~Account( void )
 //methods
 void	Account::makeDeposit( int deposit )
 {
+	int p_amount = this->_amount;
+	this->_amount += deposit;
+	this->_nbDeposits++;
+	
+	_totalNbDeposits++;
+    _totalAmount += deposit;
+
+	_displayTimestamp();
+	std::cout
+	<< "index:" <<  this->_accountIndex << ";"
+	<< "p_amount:" << p_amount << ";"
+	<< "deposit:" << deposit << ";"
+	<< "amount:" << this->_amount << ";"
+	<< "nb_deposits:" << this->_nbDeposits
+	<<std::endl;
+	
 	
 }
 bool	Account::makeWithdrawal( int withdrawal )
-{
+{	
+	_displayTimestamp();
+	
+	std::cout
+	<< "index:" <<  this->_accountIndex << ";"
+	<< "p_amount:" << this->_amount << ";";
+
+	if (this->checkAmount() < withdrawal)
+	{
+		std::cout
+		<< "withdrawal:" << "refused"
+		<<std::endl;
+		return (false);
+	}
+	
+	this->_amount -= withdrawal;
+	this->_nbWithdrawals++;
+	_totalNbWithdrawals++;
+    _totalAmount -= withdrawal;
+
+	std::cout
+	<< "withdrawal:" << withdrawal << ";"
+	<< "amount:" << this->_amount << ";"
+	<< "nb_withdrawals:" << this->_nbWithdrawals
+	<<std::endl;
+
+	return (true);
 	
 }
 int		Account::checkAmount( void ) const
 {
-	
+	return (this->_amount);
 }
 void	Account::displayStatus( void ) const
 {
+	_displayTimestamp();
+	std::cout
+	<< "index:" <<  this->_accountIndex << ";"
+	<< "amount:" << this->_amount << ";"
+	<< "deposits:" << this->_nbDeposits << ";"
+	<< "withdrawals:" << this->_nbWithdrawals 
+	<<std::endl;
+	
 	
 }
 
-void	Account::_displayTimestamp( void )
-{
-	
+void Account::_displayTimestamp(){
+    std::time_t result = std::time(NULL);
+
+    std::cout << std::setfill('0') <<"[" << 1900 + std::localtime(&result)->tm_year
+                << std::setw(2) << 1 + std::localtime(&result)->tm_mon
+                <<  std::setw(2) << std::localtime(&result)->tm_mday
+                <<  "_"
+                <<  std::setw(2) << std::localtime(&result)->tm_hour
+                <<  std::setw(2) << std::localtime(&result)->tm_min
+                <<  std::setw(2) << std::localtime(&result)->tm_sec 
+                << "] " << std::flush;
 }
